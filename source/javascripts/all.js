796de00ce6a8af1df28899b5f5c0a39f7974c0ca
Lucas Jenss <lucas@x3ro.de>
2015-03-25 20:46:57 +0100

    Make Shift+D toggle debug grid

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
 //= require_tree .
+
+// Make Shift+D toggle debug grid
+document.onkeydown = function(e) {
+    e = e || window.event
+    var k = KeyCode;
+    var handled = false;
+    if(k.hot_key(k.translate_event(e)) === "Shift+D") {
+        handled = true;
+        document.querySelector('body').classList.toggle('debug');
+    }
+    if(handled && e.preventDefault) {
+        e.preventDefault();
+        return false;
+    }
+};
